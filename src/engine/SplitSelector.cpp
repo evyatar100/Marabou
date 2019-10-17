5a234657d8445d7396e99b0acb8954232c3d6513
evyatar100 <evyatar100>
2019-10-18 01:26:11 +0300

    add prints to look for segmentation fault

--- src/engine/SplitSelector.cpp
+++ src/engine/SplitSelector.cpp
@@ -45,24 +45,38 @@ PiecewiseLinearConstraint *SplitSelector::getNextConstraint()
 void SplitSelector::logPLConstraintSplit( PiecewiseLinearConstraint *constraintForSplitting, int numVisitedTreeStates )
 {
     std::cout << "start SS logPLConstraintSplit" << std::endl;
+
     ASSERT( _constraint2OpenLogEntry.find( constraintForSplitting ) != _constraint2OpenLogEntry.end() );
     ASSERT( _constraint2OpenLogEntry[constraintForSplitting] == nullptr );
+
+    std::cout << "1 SS logPLConstraintSplit" << std::endl;
+
 
     LogEntry *logEntry = new LogEntry( _numOfConstraints );
     logEntry->splittedConstraint = _constraint2index[constraintForSplitting];
     logEntry->numVisitedTreeStatesAtSplit = numVisitedTreeStates;
 
+    std::cout << "2 SS logPLConstraintSplit" << std::endl;
+
     for ( auto constraint: *_violatedPlConstraints )
     {
+        std::cout << constraint << " 3 SS logPLConstraintSplit" << std::endl;
+
         int i = _constraint2index[constraint];
         logEntry->isActive[i] = constraint->isActive();
     }
 
+    std::cout << "4 SS logPLConstraintSplit" << std::endl;
+
+
     for ( auto constraint: *_violatedPlConstraints )
     {
+        std::cout <<constraint<< "  5 SS logPLConstraintSplit" << std::endl;
+
         int i = _constraint2index[constraint];
         logEntry->isViolated[i] = true;
     }
+    std::cout << " 6 SS logPLConstraintSplit" << std::endl;
 
     _constraint2OpenLogEntry[constraintForSplitting] = logEntry;
     _log.push_back( logEntry );
