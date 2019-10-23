475507921e3dd6b8a5b305d9766378f8cd046844
evyatar100 <evyatar100>
2019-10-23 14:10:40 +0300

    fix fix commas in strings

--- src/engine/SplitSelector.cpp
+++ src/engine/SplitSelector.cpp
@@ -47,7 +47,6 @@ SplitSelector::SplitSelector( List<PiecewiseLinearConstraint *> plConstraints )
 
     _fout.open(CSV_FILE_PATH, std::ios::out);
     _fout.close();
-    exit(0);
     _fout.open(CSV_FILE_PATH, std::ios::out);
     writeHeadLine();
 }
