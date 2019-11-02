2bebdbad4c6150bafc705058e658493046fba1e0
evyatar100 <evyatar100>
2019-11-03 00:38:15 +0200

    cancel last commit

--- src/engine/SplitSelector.cpp
+++ src/engine/SplitSelector.cpp
@@ -39,7 +39,7 @@ SplitSelector::SplitSelector( List<PiecewiseLinearConstraint *> plConstraints )
         , _fout()
         , _csvPath()
 {
-    _generator = std::default_random_engine( static_cast<long unsigned int>(time( 0 )) + getpid());
+    _generator = std::default_random_engine( static_cast<long unsigned int>(time( 0 )) );
 
     std::cout << "start SS constructor" << '\n';
     int i = 0;
