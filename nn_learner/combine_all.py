8edde87a67cd75a5b61e0f5676a0b050f54b5793
Evyatar Cohen <evyatar100@cs.huji.ac.il>
2019-11-03 14:43:23 +0200

    finish combine_all.py

--- nn_learner/combine_all.py
+++ nn_learner/combine_all.py
@@ -7,21 +7,27 @@ default_name = "combined.csv"
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("enter path to dir")
+        print(f'USE: .py csv_files_dir new_csv(optional, default = {default_name}')
         exit(1)
 
     dfs = []
     os.chdir(sys.argv[1])
-
-    for file in os.listdir('.'):
-        if file.endswith(".csv"):
-            dfs.append(pd.read_csv(file))
-
-    big_frame = pd.concat(dfs)
 
     if len(sys.argv) <= 2:
         name = default_name
     else:
         name = sys.argv[2]
 
+    os.remove(name)
+
+    for file in os.listdir('.'):
+        if file.endswith(".csv"):
+            print(file)
+            dfs.append(pd.read_csv(file))
+
+    big_frame = pd.concat(dfs)
+
     big_frame.to_csv(name, index=False)
+
+    print("done..")
+    print(f'new file was created: {name}')
---
+++ run.sh
@@ -0,0 +1,1 @@
+src/engine/marabou.elf resources/nnet/acasxu/ACASXU_experimental_v2a_2_7.nnet resources/properties/acas_property_3.txt
